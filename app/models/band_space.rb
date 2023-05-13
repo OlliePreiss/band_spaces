e638766248e5ffed9df7ab64022939043f650209
Ollie Preiss <76745659+OlliePreiss@users.noreply.github.com>
2023-05-13 15:45:48 +0100

    updates to band spaces model

--- app/models/band_space.rb
+++ app/models/band_space.rb
@@ -3,7 +3,9 @@ class BandSpace < ApplicationRecord
 
   validates :name, presence: true
   validates :address, presence: true
+  validates :address, length: { minimum: 10, maximum: 100 }
   validates :description, presence: true
+  validates :description, length: { minimum: 10, maximum: 100 }
   validates :photo_url, presence: true
   validates :users_id, presence: true
   validates :price, presence: true
