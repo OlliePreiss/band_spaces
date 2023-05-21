d4e5e14a07e28d2640ecc9b275739e9c1cc36708
Adam Steele <adamsteele@hotmail.co.uk>
2023-05-21 16:46:58 +0100

    changed date field on booking to html5 as Juan mentioned, fixed the username issue where this didn't save correctly on sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :email)
+    end
+  end
 end
